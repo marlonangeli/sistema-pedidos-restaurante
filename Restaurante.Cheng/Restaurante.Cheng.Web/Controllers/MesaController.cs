0a7009e4ede7683a15d78baba43ef23ed5023d14
gabrieldamke <75023146+gabrieldamke@users.noreply.github.com>
2023-04-10 11:29:29 -0300

    add latest frontend updates

--- Restaurante.Cheng/Restaurante.Cheng.Web/Controllers/MesaController.cs
+++ Restaurante.Cheng/Restaurante.Cheng.Web/Controllers/MesaController.cs
@@ -1,26 +1,42 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Restaurante.Cheng.Web.Models;
+using Restaurante.Cheng.Domain.Interfaces;
+using Restaurante.Cheng.Domain.Enums;
+using Restaurante.Cheng.Domain.Entities;
 
 namespace Restaurante.Cheng.Web.Controllers;
 
 public class MesaController : Controller
 {
     private readonly ILogger<MesaController> _logger;
+    private readonly IRepository<Mesa> _mesaRepository;
 
-    public MesaController(ILogger<MesaController> logger)
+    public MesaController(ILogger<MesaController> logger, IRepository<Mesa> mesaRepository)
     {
         _logger = logger;
+        _mesaRepository = mesaRepository;
     }
 
-    public IActionResult Index()
+    [HttpGet]
+    public async Task<IActionResult> Index()
     {
-        return View();
+
+        var mesas = await _mesaRepository.GetAllAsync();
+        return View(mesas);
+    }
+
+    public IActionResult Edit(int id)
+    {
+        Console.WriteLine("teste");
+        return PartialView("~/Views/Mesa/Edit.cshtml");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
     }
 }
