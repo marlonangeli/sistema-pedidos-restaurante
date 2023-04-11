0a4cd10de188c8f790c8772bb6dd4095724da29f
Marlon Angeli <marlonangeli91@gmail.com>
2023-04-10 21:57:38 -0300

    update database

---
+++ Restaurante.Cheng/Restaurante.Cheng.Data/Migrations/20230411004647_UpdateEntities.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurante.Cheng.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GarcomId1",
+                table: "Atendimentos",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MesaId1",
+                table: "Atendimentos",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Atendimentos_GarcomId1",
+                table: "Atendimentos",
+                column: "GarcomId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Atendimentos_MesaId1",
+                table: "Atendimentos",
+                column: "MesaId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Atendimentos_Garcons_GarcomId1",
+                table: "Atendimentos",
+                column: "GarcomId1",
+                principalTable: "Garcons",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Atendimentos_Mesas_MesaId1",
+                table: "Atendimentos",
+                column: "MesaId1",
+                principalTable: "Mesas",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Atendimentos_Garcons_GarcomId1",
+                table: "Atendimentos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Atendimentos_Mesas_MesaId1",
+                table: "Atendimentos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Atendimentos_GarcomId1",
+                table: "Atendimentos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Atendimentos_MesaId1",
+                table: "Atendimentos");
+
+            migrationBuilder.DropColumn(
+                name: "GarcomId1",
+                table: "Atendimentos");
+
+            migrationBuilder.DropColumn(
+                name: "MesaId1",
+                table: "Atendimentos");
+        }
+    }
+}
---
+++ Restaurante.Cheng/Restaurante.Cheng.Domain/Entities/AtendimentoProduto.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurante.Cheng.Domain.Entities;
+
+public class AtendimentoProduto
+{
+    public int AtendimentoId { get; set; }
+    public Atendimento Atendimento { get; set; }
+    public int ProdutoId { get; set; }
+    public Produto Produto { get; set; }
+}
